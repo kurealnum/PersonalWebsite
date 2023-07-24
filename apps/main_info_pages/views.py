62254db81f37703f734518bb12b0c3ca20caae04
kurealnum <cattledoger@gmail.com>
2023-07-24 11:39:19 -0400

    added links to github cards

--- apps/main_info_pages/views.py
+++ apps/main_info_pages/views.py
@@ -16,14 +16,15 @@ def index(request):
     for repo in repo_list:
         #get all the data
         repo_data = requests.get(f"https://api.github.com/repos/kurealnum/{repo}").json()
+        repo_link = repo_data["html_url"]
         name = repo_data["full_name"]
         desc = repo_data["description"]
         lang = repo_data["language"]
         star_count = repo_data["stargazers_count"]
         fork_count = repo_data["forks_count"]
 
-        github_repos[name] = [desc, lang, star_count, fork_count]
-        # (index) 0 is description, 1 is the language, 2 is the star count, and 3 is the fork count
+        github_repos[name] = [desc, lang, star_count, fork_count, repo_link]
+        # (index) 0 is description, 1 is the language, 2 is the star count, 3 is the fork count, 4 is the link
 
     context = {
         "github_repos": github_repos,
