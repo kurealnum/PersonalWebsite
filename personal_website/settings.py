57d6dcb03172908d764c18b49112b640432cf78c
kurealnum <cattledoger@gmail.com>
2024-01-04 15:37:52 -0500

    Small updates to my website

--- personal_website/settings.py
+++ personal_website/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9c%l)fv5f8+=#zeojv*9(1dp%yyk8n%b_gk6l!k+25b*-8i&v)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
